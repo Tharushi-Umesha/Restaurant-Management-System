30135b304c4f9decd46d304df2c437a333aa044f
Tharushi Umesha Mahipala <145181808+Tharushi-Umesha@users.noreply.github.com>
2025-08-24 16:10:35 +0530

    feat: create the registration form completed and add user

--- RegisterForm.Designer.cs
+++ RegisterForm.Designer.cs
@@ -86,6 +86,7 @@
             this.register_btn.TabIndex = 18;
             this.register_btn.Text = "REGISTER";
             this.register_btn.UseVisualStyleBackColor = false;
+            this.register_btn.Click += new System.EventHandler(this.register_btn_Click);
             // 
             // register_showPassword
             // 
--- RegisterForm.cs
+++ RegisterForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace RestaurantManagementSystem
 {
     public partial class RegisterForm : Form
     {
+
+        string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\tharushi umesha\OneDrive - Edith Cowan University\Documents\RestaurantManagementSystem.mdf"";Integrated Security=True;Connect Timeout=30";
         public RegisterForm()
         {
             InitializeComponent();
@@ -37,6 +40,55 @@ namespace RestaurantManagementSystem
         {
             register_password.PasswordChar = register_showPassword.Checked ? '\0' : '*';
             register_confirmPassword.PasswordChar = register_showPassword.Checked ? '\0' : '*';
+        }
+
+        private void register_btn_Click(object sender, EventArgs e)
+        {
+            using(SqlConnection conn = new SqlConnection(connection))
+            {
+                conn.Open();
+
+                string checkUsernameQuery = "SELECT * FROM users WHERE username = @username";
+
+                using (SqlCommand checkUsername = new SqlCommand(checkUsernameQuery, conn))
+                {
+                    checkUsername.Parameters.AddWithValue("@username", register_username.Text.Trim());
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(checkUsername);
+                    DataTable table = new DataTable();
+
+                    adapter.Fill(table);
+
+                    if(table.Rows.Count != 0)
+                    {
+                        MessageBox.Show($"{register_username.Text.Trim()} was already taken", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                    }else if(register_password.Text.Trim().Length < 8)
+                    {
+                        MessageBox.Show("Invalid Password, at least 8 characters needed", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error );
+                    }else if(register_password.Text.Trim() != register_confirmPassword.Text.Trim())
+                    {
+                        MessageBox.Show("Passwords are not matching", "Error Message",  MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        string insertData = "INSERT INTO users (username, password, status, date_created) VALUES (@username, @password, @status, @date)";
+
+                        using(SqlCommand cmd = new SqlCommand(insertData, conn))
+                        {
+                            cmd.Parameters.AddWithValue ("@username", register_username.Text.Trim());
+                            cmd.Parameters.AddWithValue ("@password", register_password.Text.Trim());
+                            cmd.Parameters.AddWithValue ("@status", "Active");
+
+                            DateTime today = DateTime.Now;
+                            cmd.Parameters.AddWithValue ("@date", today);
+
+                            cmd.ExecuteNonQuery();
+
+                            MessageBox.Show("Register User Successfully.", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
         }
     }
 }
